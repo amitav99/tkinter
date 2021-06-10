7702cf4ff68954aaef3147058e45adb7242cd356
Amitav <amitavdebnath2000@gmail.com>
2021-06-11 00:39:32 +0600

    add again

--- tkinter7.py
+++ tkinter7.py
@@ -1,14 +1,27 @@
 # gridlines
 from tkinter import *
 
+
 root = Tk()
 root.geometry("655x434")
 
-frame = Frame(root, borderwidth=5, bg='black', relief= SUNKEN)
-frame.pack()
+# frame = Frame(root, borderwidth=5, bg='black', relief= SUNKEN)
+# frame.pack()
 user = Label(root, text="username")
-user.pack()
+user.grid()
+
 password = Label(root, text="password")
-password.pack()
+password.grid(row=1)
+#variable class in tkinter
+#BooleanVar, DoubleVar, IntVar, StringVar
+
+uservalue= StringVar()
+passvalue=StringVar()
+
+userentry= Entry(root, textvariable= uservalue)
+passentry= Entry(root, textvariable= passvalue)
+
+userentry.grid(row=0,column=1)
+passentry.grid(row=1,column=1)
 
 root.mainloop()
